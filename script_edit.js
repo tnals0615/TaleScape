ebcdd8f2caea5ccc3a4b985c1c8bdcc7dd4d034b
h10119kim <h10119k@gmail.com>
2024-12-13 02:36:00 +0900

    위지위그그

--- script_edit.js
+++ script_edit.js
@@ -3,3 +3,12 @@ const iconMoon = document.getElementById("moon");
 iconMoon.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 });
+
+tinymce.init({
+  selector: "#wysiwyg-editor",
+  menubar: false,
+  plugins: "lists link image preview",
+  toolbar:
+    "undo redo | formatselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image preview",
+  height: 500,
+});
